e10c2ef038bfed773c2e8a666a04e5a8c657b961
Lynabch <42008865@parisnanterre.fr>
2025-01-10 21:13:46 +0100

    V1 Front- page d'accueil

--- ADP/views.py
+++ ADP/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+def accueil(request):
+    # Liste des types d'activités pour le menu déroulant
+    activites = ["Bibliothèque", "Parc", "Restaurant", "Coworking"]
+    context = {
+        'activites': activites,
+        'image_paris': 'aDeuxPas/static/images/image_paris.jpeg',  # Chemin vers ton image
+    }
+    return render(request, 'aDeuxPas/accueil.html', context)
+from django.shortcuts import render
+
 # Create your views here.
--- aDeuxPas/settings.py
+++ aDeuxPas/settings.py
@@ -119,10 +119,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (css, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
